71d897eef0c4c0a6e05fa3b7b8041e72eae83cb3
dgmorr <dgmorrison@uchicago.edu>
2023-02-28 13:31:20 -0800

    add backprop code to src

--- src/training.py
+++ src/training.py
@@ -1,24 +1,61 @@
-# Cost function for an individual training example
-def squared_error_f(ex, ac):
-    assert len(ex) == len(ac)
-    diff_sq = [(ex_i - ac_i) ** 2 for (ex_i, ac_i) in zip(ex, ac)]
-    return 0.5 * sum(diff_sq)
-# Derivative WRT particular activation ac_i
-def squared_error_fprime(ex_i, ac_i):
-    return ac_i - ex_i
+# t = training set of the form [(x, y)]
+def mse(t):
+    total_error = 0
+    for (x, y) in t:
+        total_error += squared_error(x, y, net)
+    return total_error / len(t)
 
-# 1. Error for each output neuron
-def output_error(net, ex)
-    delta_L = []
-    for j in range(len(net.layers[-1])):
-        neuron = net.layers[-1][j]
-        a_j = neuron.current_value
-        z_j = neuron.current_z
-        y_j = ex[j]
-        delta_j = (a_j - y_j) * neuron.activation.deriv(z_j)
-        delta_L.append(delta_j)
-    return delta_L
-        
+def squared_error(x, y, net):
+    a = net.passthrough(x)
+    return 0.5 * sum([(a[i] - y[i])**2 for i in range(len(a))])
+
+def squared_error_prime(aL, y):
+    return [aL[i] - y[i] for i in range(len(aL))]
+
+# x and y are a training example
+# cost_x_prime is an arbitary single-example cost function derivative 
+# that takes an output vector a^L and an expected vector y
+def final_layer_error(net, x, y, cost_x_prime):
+    aL = net.passthrough(x)
+    dC_da = cost_x_prime(aL, y)
+    sigma_prime_zL = [neuron.activation.deriv(neuron.pre_activation_value) for neuron in net.layers[-1]]
+    deltaL = [dC_da[i] * sigma_prime_zL[i] for i in range(len(dC_da))]
+    return deltaL
+
+def layer_error(net, x, y, cost_x_prime):
+    deltaL = final_layer_error(net, x, y, cost_x_prime)
+    error_vectors = [deltaL]
+    for l in range(2, net.n_layers):
+        layer = net.layers[-l]  # start with layers[-2], work back to layers[-n_layers + 1]
+        # First layer does not have an error associated with it because it doesn't have input weights
+        weights_transpose = [neuron.downstream_weights for neuron in layer]
+        next_delta = error_vectors[0]
+        weight_delta_product = []
+        for weights in weights_transpose:
+            weight_delta_product.append(sum([weights[i] * next_delta[i] for i in range(len(next_delta))]))
+        sigma_prime_zl = [neuron.activation.deriv(neuron.pre_activation_value) for neuron in layer]
+        deltal = [weight_delta_product[i] * sigma_prime_zl[i] for i in range(len(sigma_prime_zl))]
+        error_vectors = [deltal] + error_vectors
+    return error_vectors
+
+# No need for (3) as bias gradient for each layer is just the error vector for that layer
+def weight_gradient(net, error_vectors):
+    weight_derivs = []
+    for l in range(1, net.n_layers):
+        deltal = error_vectors[l - 1]
+        layer = net.layers[l]  # No need to go backwards this time
+        prev_layer = net.layers[l - 1]
+        layer_derivs = []
+        for j in range(len(layer)):
+            layer_derivs_j = []
+            for k in range(len(prev_layer)):
+                prev_a_k = prev_layer[k].activation_value
+                deltal_j = deltal[j]
+                dC_dwjk = prev_a_k * deltal_j
+                layer_derivs_j.append(dC_dwjk)
+            layer_derivs.append(layer_derivs_j)
+        weight_derivs.append(layer_derivs)
+    return weight_derivs
 
 
     
